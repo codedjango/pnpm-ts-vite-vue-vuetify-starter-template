34fc65902cc89fbd0985a4289a0dbbf4134bbe09
vuedj <aryamane.aniket@gmail.com>
2026-08-05 11:56:43 +0530

    Initial Commit

---
+++ src/main.ts
@@ -0,0 +1,19 @@
+import { createApp } from 'vue'
+
+import '@mdi/font/css/materialdesignicons.css'
+
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+import App from '@/App.vue'
+
+import 'unfonts.css'
+import 'vuetify/styles'
+
+const vuetify = createVuetify({
+	components,
+	directives,
+})
+
+createApp(App).use(vuetify).mount('#app')
---
+++ vite.config.ts
@@ -0,0 +1,45 @@
+import path from 'path'
+
+import vue from '@vitejs/plugin-vue'
+import ViteFonts from 'unplugin-fonts/vite'
+import { defineConfig } from 'vite'
+import VueDevTools from 'vite-plugin-vue-devtools'
+import vuetify from 'vite-plugin-vuetify'
+
+// https://vite.dev/config/
+export default defineConfig({
+	plugins: [
+		vue(),
+		vuetify({ autoImport: true }),
+		VueDevTools(),
+		ViteFonts({
+			fontsource: {
+				families: [
+					{
+						name: 'Roboto',
+						weights: [100, 300, 400, 500, 700, 900],
+						styles: ['normal', 'italic'],
+					},
+				],
+			},
+		}),
+	],
+	resolve: {
+		alias: {
+			'@': path.resolve(import.meta.dirname, './src'),
+		},
+	},
+	// server: {
+	// 	proxy: {
+	// 		'/api': {
+	// 			target: 'http://127.0.0.1:8000',
+	// 			changeOrigin: true,
+	// 			// rewrite: (r) => r.replace(/^\/api/, ''),
+	// 			headers: {
+	// 				Accept: 'application/json',
+	// 				'Content-Type': 'application/json',
+	// 			},
+	// 		},
+	// 	},
+	// },
+})
